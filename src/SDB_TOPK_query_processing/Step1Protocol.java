00af375a975235c7905b4e91cea6f53543b20541
Mahboubi Sakina <mahboubisakina@yahoo.com>
2017-11-22 14:49:24 +0100

    Update Step1Protocol.java

--- src/SDB_TOPK_query_processing/Step1Protocol.java
+++ src/SDB_TOPK_query_processing/Step1Protocol.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basic_approach_one_msg.core;
 
 import basic_approach_one_msg.core.DataNode;
@@ -21,16 +17,9 @@ import peersim.core.Node;
 import peersim.edsim.EDProtocol;
 import peersim.transport.Transport;
 
-/**
- *
- * @author sakina
- */
 public class Step1Protocol implements EDProtocol{
-    
-    
     public Step1Protocol(String prefix){   
     }
-    
     public Object clone(){
         return new Step1Protocol("") ;
     }
@@ -43,31 +32,18 @@ public class Step1Protocol implements EDProtocol{
            
             double TH;
             MasterDataNode master = (MasterDataNode) Network.get(0);
-            //System.err.println("node "+"le resultat est " + CommonState.getTime());
-        if (event instanceof Message3) {
-             
-            //System.out.println(" -------------------protocol1-----------------");
+            if (event instanceof Message3) {
             Message3 msg = (Message3) event;
             if(msg.getSender() != null) {
-                //System.err.println("node "+node.getIndex()+" receive msg from master 0 at " + CommonState.getTime());
                 double begin = System.currentTimeMillis();
                 Bucket_description result = ((DataNode)node).getDataIDinNextBucket();
                 double end = System.currentTimeMillis();
                 Message2 reply = new Message2(result, node, (end-begin));
-               
-                //System.err.println("le temps écoulé pour calculer le résultat est " + CommonState.getTime());
-                
                 ((Transport)node.getProtocol(FastConfig.getTransport(pid))).send(node, master, reply, pid);
-                //System.out.println("-----------------latency between "+ node.getIndex()+" and "+ master.getIndex()+ " is "+((Transport)node.getProtocol(FastConfig.getTransport(pid))).getLatency(node, master));
-                
-                //System.err.println("le temps écoulé pour répondre au 1ier message est " + CommonState.getTime());
-                //System.out.println("Data sent from " + node.getIndex() + " to " + master.getIndex()+" at "+CommonState.getTime());
-                
             }             
         }
         else{
             if (event instanceof Message2) {
-                //System.out.println(" -------------------protocol1-----------------");
                 if(!(node instanceof MasterDataNode)) return;
                 Message2 msg = (Message2) event;
                 if (msg.getCalcul_time()>master.getMax_time()) {
@@ -77,18 +53,8 @@ public class Step1Protocol implements EDProtocol{
                 master.putStep1Result((Bucket_description)msg.getValue(), msg.getSender().getIndex());
                 double end = System.currentTimeMillis();
                 master.setProcess_time(master.getProcess_time()+(end-begin));
-                //System.err.println("master receive the response from "+msg.getSender().getIndex()+" at " + CommonState.getTime());
-                //master.printdataCollection();
                 if(master.getNbrResponses()%Network.size()==0) { // Everyone responded
-//                    for (int i = 0; i < master.getCollectedData().size(); i++) {
-//                        System.out.println("collected data : ids"+Arrays.toString(((Bucket_description)master.getCollectedData().get(i)).dataids)+
-//                                "  min"+Arrays.toString(((Bucket_description)master.getCollectedData().get(i)).bucket_min));
-//                    }
-                    
-                    // Process data
-                    //System.out.println("Everyone responded! process data and start Step2 ");
-                    
-                    SimulationController controller = (SimulationController)Configuration.getInstance(Configuration.getNames("control")[0]);
+                   SimulationController controller = (SimulationController)Configuration.getInstance(Configuration.getNames("control")[0]);
                     begin = System.currentTimeMillis();
                     ArrayList list = master.getCollectedData();
                     end = System.currentTimeMillis();
@@ -98,20 +64,10 @@ public class Step1Protocol implements EDProtocol{
                     master.setMax_time(0);
                     master.setProcess_time(0);
                     master.dataCollectionReinitialzation();
-                    
-                    
-                    //this.startStep2(master);
-                    
-                     
-                        
                 }
             }
         }            
                     
         }           
-                    
-                    
-
-        
-        
+                 
 }
