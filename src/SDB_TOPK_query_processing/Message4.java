bd2268a23dc6279995f5757bd3e5dd7ad65e2c95
Mahboubi Sakina <mahboubisakina@yahoo.com>
2017-11-22 14:38:44 +0100

    Update Message4.java

--- src/SDB_TOPK_query_processing/Message4.java
+++ src/SDB_TOPK_query_processing/Message4.java
@@ -1,17 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basic_approach_one_msg.core;
 
 import java.util.Set;
 import peersim.core.Node;
 
-/**
- *
- * @author sakina
- */
+
 public class Message4 {
     final String[] value;
     
