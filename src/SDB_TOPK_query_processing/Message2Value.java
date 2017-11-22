56c80e1813fedb5b5e4c3e87576784268985cc7c
Mahboubi Sakina <mahboubisakina@yahoo.com>
2017-11-22 14:38:07 +0100

    Update Message2Value.java

--- src/SDB_TOPK_query_processing/Message2Value.java
+++ src/SDB_TOPK_query_processing/Message2Value.java
@@ -1,15 +1,6 @@
 package basic_approach_one_msg.core;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author sakina
- */
 public class Message2Value {
     String id;
     double min_bound;
