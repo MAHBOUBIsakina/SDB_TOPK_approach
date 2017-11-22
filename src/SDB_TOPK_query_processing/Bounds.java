3fa45f658e218342ade982b68c227579e5f21004
Mahboubi Sakina <mahboubisakina@yahoo.com>
2017-11-22 11:17:43 +0100

    Update Bounds.java

--- src/SDB_TOPK_query_processing/Bounds.java
+++ src/SDB_TOPK_query_processing/Bounds.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basic_approach_one_msg.core;
 
-/**
- *
- * @author sakina
- */
 public class Bounds {
     double []min_bound;
     double []max_bound;
