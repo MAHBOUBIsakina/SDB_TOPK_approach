30d25db119b7455f9ffa13f042889395461d7b4f
Mahboubi Sakina <mahboubisakina@yahoo.com>
2017-11-22 11:18:43 +0100

    Update DataItem.java

--- src/SDB_TOPK_query_processing/DataItem.java
+++ src/SDB_TOPK_query_processing/DataItem.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package basic_approach_one_msg.core;
 
 import peersim.config.Configuration;
 
-/**
- *
- * @author sakina
- */
 public class DataItem {
     String id;
     Bounds [] bound;
